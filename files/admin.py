a271bfe11538bb0b4f15867da8fab61698a139e9
Bob Belderbos <bobbelderbos@gmail.com>
2024-02-13 14:26:33 +0100

    init commit

---
+++ files/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Attachment
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('url', 'added')
+    search_fields = ('url',)
